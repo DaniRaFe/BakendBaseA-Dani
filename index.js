cfe9f7109f4e70679b47461b9ebc7a1ad178fab4
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2022-09-14 10:31:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+app.listen(4000, () => {
+    console.log('Backend en ejecucion')
+})
