e5ca5b179a7e25891501f1d29859178be2cdb218
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2022-09-22 10:56:38 -0500

    first commit

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,14 @@
 const {Router} = require("express")
 const router = Router()
+const {
+    rootMessage,
+    hiMessage, 
+    byeMessage} 
+    = require('../controllers/messages')
 
 //router.get("", () => {})
-router.get("/", (req, res) => {res.send("Hola!")}) //End Point
+router.get('/', rootMessage) //End Point
+router.get('/hi', hiMessage) //End Point
+router.get('/bye', byeMessage) //End Point
 
 module.exports = router
