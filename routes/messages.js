4066ef3c4d732505d6b6f3c35f6c55a018e741de
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2022-09-23 11:07:42 -0500

    Añadiendo PUT, DELETE Y POST

--- routes/messages.js
+++ routes/messages.js
@@ -3,12 +3,20 @@ const router = Router()
 const {
     rootMessage,
     hiMessage, 
-    byeMessage} 
+    byeMessage,
+    postMessage,
+    putMessage,
+    deleteMessage
+} 
     = require('../controllers/messages')
 
 //router.get("", () => {})
 router.get('/', rootMessage) //End Point
 router.get('/hi', hiMessage) //End Point
 router.get('/bye', byeMessage) //End Point
+
+router.post('/', postMessage)
+router.put('/', putMessage)
+router.delete('/', deleteMessage)
 
 module.exports = router
