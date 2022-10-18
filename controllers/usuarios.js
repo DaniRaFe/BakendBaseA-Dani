2e92180532e66ad86dc0973c6c7a8d060c33f575
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2022-10-18 10:38:43 -0500

    Nueva consulta

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,60 @@
-const {request, response} = require("express");
+const { request, response } = require("express");
+const pool = require("../db/connection")
 
-const getUsers = (req = request, res = response) => {
-    console.log("Funcion getUsers")
-    res.json({msg: "Funcion GetUsers"})
+const getUsers = async (req = request, res = response) =>{
+    //estructura basica de cualquier endpoint al conectar en su BD
+    
+    let conn;
+    //control de exepciones
+    try {
+        conn = await pool.getConnection()
+        //esta es la consulta mas basica, se pueden hacer mas complejas
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error) })
+        //siempre validar que no se obtuvieron resultados
+        if (!users) {
+            res.status(404).json({msg:"no se encontraron registros"})
+            return
+        }
+        res.json({users})
+        //lo del cath y final siempre sera lo mismo
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
 }
 
-module.exports = {getUsers}
+
+
+
+const getUserByID = async (req = request, res = response) =>{
+    const {id} = req.params
+    //estructura basica de cualquier endpoint al conectar en su BD
+    
+    let conn;
+    //control de exepciones
+    try {
+        conn = await pool.getConnection()
+        //esta es la consulta mas basica, se pueden hacer mas complejas
+        const [user] = await conn.query(`SELECT * FROM Usuarios WHERE ID = ${id}`, (error) => {throw new Error(error) })
+        //siempre validar que no se obtuvieron resultados
+        if (!user) {
+            res.status(404).json({msg:`no se encontro registro con el ID ${id}`})
+            return
+        }
+        res.json({user})
+        //lo del cath y final siempre sera lo mismo
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
+}
+
+module.exports = {getUsers, getUserByID}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,10 @@
 const {Router} = require("express")
-const {getUsers} = require("../controllers/usuarios")
+const {getUsers, getUserByID} = require("../controllers/usuarios")
 const router = Router()
 
 //vamos a definir las rutas del localhost http://localhost:4005/api/v1/usuarios
 
 router.get("/", getUsers)
+router.get("/id/:id", getUserByID)
 
 module.exports = router
