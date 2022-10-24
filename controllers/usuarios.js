259b179c028f494d1a176c6d3d63f70795138063
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2022-10-24 10:59:16 -0500

    eliminar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -57,4 +57,31 @@ const getUserByID = async (req = request, res = response) =>{
     }
 }
 
-module.exports = {getUsers, getUserByID}
+const deleteUserByID = async (req = request, res = response) =>{
+    const {id} = req.query
+    //estructura basica de cualquier endpoint al conectar en su BD
+    
+    let conn;
+    //control de exepciones
+    try {
+        conn = await pool.getConnection()
+        //esta es la consulta mas basica, se pueden hacer mas complejas
+        const {affectedRows} = await conn.query(`UPDATE Usuarios SET Activo = 'N' WHERE ID = ${id}`, (error) => {throw new Error(error) })
+        //siempre validar que no se obtuvieron resultados
+       if (affectedRows ===0) {
+            res.status(404).json({msg:`no se pudo eliminar el registro con el ID ${id}`})
+            return
+        }
+        res.json({msg: `El usuario con id ${id} se elimino correctamente.`})
+        //lo del cath y final siempre sera lo mismo
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }finally{
+        if(conn){
+            conn.end()
+        }
+    }
+}
+
+module.exports = {getUsers, getUserByID, deleteUserByID}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,13 @@
+
 const {Router} = require("express")
-const {getUsers, getUserByID} = require("../controllers/usuarios")
+const {getUsers, getUserByID, deleteUserByID} = require("../controllers/usuarios")
 const router = Router()
 
 //vamos a definir las rutas del localhost http://localhost:4005/api/v1/usuarios
+// http://localhost:4005/api/v1/usuarios?id=6
 
 router.get("/", getUsers)
 router.get("/id/:id", getUserByID)
+router.delete("/", deleteUserByID)
 
 module.exports = router
